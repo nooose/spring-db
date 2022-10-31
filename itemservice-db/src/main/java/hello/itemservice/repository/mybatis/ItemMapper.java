33830f37f85650ae0ea0d9f419626611ff5112e8
nooose <hd15807@gmail.com>
2022-11-01 00:16:02 +0900

    MyBatis 어노테이션 맛보기

--- itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -5,6 +5,7 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ public interface ItemMapper {
 
     void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateDto);
 
+    // @Select("select id, item_name, price, quantity fro item where id=#{id}")
     Optional<Item> findById(Long id);
 
     List<Item> findAll(ItemSearchCond itemSearchCond);
